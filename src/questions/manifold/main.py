f0db0487475402ac0b496debf59dba1f02ff6d2b
Houtan Bastani <houtan@forecastingresearch.org>
2024-03-22 14:59:06 -0400

    manifold: get most popular markets for various topic slugs
    Get:
    * overall most popular
    * entertainment
    * sports
    * technology
    
    Remove duplicate markets.
    
    Place datetime closer to response from API.
    
    Not storing the Manifold tags because some are quite specific. Prefer to coherently apply tags with an LLM later.
    
    Issue #9

--- src/questions/manifold/main.py
+++ src/questions/manifold/main.py
@@ -16,6 +16,7 @@ local_market_filename = f"/tmp/{json_market_filename}"
 json_market_values_filename = "manifold_values.json"
 local_market_values_filename = f"/tmp/{json_market_values_filename}"
 bucket_name = os.environ.get("CLOUD_STORAGE_BUCKET")
+manifold_topic_slugs = ["entertainment", "sports-default", "technology-default"]
 
 
 def _get_stored_question_data():
@@ -60,7 +61,7 @@ def _get_stored_question_data():
     return dfq, dfmv
 
 
-def _get_manifold_data():
+def _get_manifold_data(topics):
     """Get the top 100 markets from Manifold Markets."""
     print("Calling Manifold search-markets endpoint")
     endpoint = "https://api.manifold.markets/v0/search-markets"
@@ -71,54 +72,78 @@ def _get_manifold_data():
         "limit": 100,
     }
     response = requests.get(endpoint, params=params)
+    utc_datetime_obj = datetime.now(timezone.utc)
     if not response.ok:
         print(f"ERROR: Request to Manifold Markets API endpoint {endpoint} failed.")
         raise
-    return response
+    tmp = [(utc_datetime_obj, response.json())]
+
+    for topic in topics:
+        response = requests.get(endpoint, params={**params, "topicSlug": topic})
+        utc_datetime_obj = datetime.now(timezone.utc)
+        if not response.ok:
+            print(f"ERROR: Request to Manifold Markets API endpoint {endpoint} failed.")
+            raise
+        tmp.append((utc_datetime_obj, response.json()))
+
+    # Remove duplicate markets
+    seen_market_ids = set()
+    unique_markets = []
+    for datetime_obj, markets in tmp:
+        unique_markets_batch = []
+        for market in markets:
+            if market["id"] not in seen_market_ids:
+                unique_markets_batch.append(market)
+                seen_market_ids.add(market["id"])
+        unique_markets.append((datetime_obj, unique_markets_batch))
+
+    return unique_markets
 
 
-def _update_questions(dfq, dfmv, response):
-    """Update the data in `dfq` given the latest Manifold market info contained in `response`."""
-    utc_datetime_obj = datetime.now(timezone.utc)
-    utc_datetime_str = utc_datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
-    utc_date_str = utc_datetime_obj.strftime("%Y-%m-%d")
+def _update_questions(dfq, dfmv, datetime_and_markets):
+    """Update the dataframes given the latest Manifold market info."""
 
-    def _get_market_value_entry(market_id, value):
+    def _get_market_value_entry(market_id, utc_datetime_str, value):
         return {
             "id": market_id,
             "datetime": utc_datetime_str,
             "value": value,
         }
 
-    def _entry_exists_for_today(market_values):
+    def _entry_exists_for_today(market_values, utc_date_str):
         return market_values["datetime"].dt.strftime("%Y-%m-%d").eq(utc_date_str).any()
 
     new_markets = []
     new_market_values = []
-    for market in response.json():
-        market_for_id = dfq[dfq["id"] == market["id"]]
-        if market_for_id.empty:
-            print(f"Adding new market `{market['id']}`")
-            new_markets.append(
-                {
-                    "id": market["id"],
-                    "question": market["question"],
-                    "resolution_criteria": (
-                        f"Resolves to the market value on {market['url']} at 12AM UTC."
-                    ),
-                    "resolved": False,
-                }
-            )
-            new_market_values.append(_get_market_value_entry(market["id"], market["probability"]))
-        else:
-            index = market_for_id.index[0]
-            if not market_for_id.at[index, "resolved"]:
-                print(f"Updating market `{market['id']}`")
-                dfq.at[index, "resolved"] = market["isResolved"]
-                if not _entry_exists_for_today(dfmv[dfmv["id"] == market["id"]]):
-                    dfmv.loc[len(dfmv)] = _get_market_value_entry(
-                        market["id"], market["probability"]
-                    )
+    for utc_datetime_obj, markets in datetime_and_markets:
+        utc_datetime_str = utc_datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+        utc_date_str = utc_datetime_obj.strftime("%Y-%m-%d")
+        for market in markets:
+            market_for_id = dfq[dfq["id"] == market["id"]]
+            if market_for_id.empty:
+                print(f"Adding new market `{market['id']}`")
+                new_markets.append(
+                    {
+                        "id": market["id"],
+                        "question": market["question"],
+                        "resolution_criteria": (
+                            f"Resolves to the market value on {market['url']} at 12AM UTC."
+                        ),
+                        "resolved": False,
+                    }
+                )
+                new_market_values.append(
+                    _get_market_value_entry(market["id"], utc_datetime_str, market["probability"])
+                )
+            else:
+                index = market_for_id.index[0]
+                if not market_for_id.at[index, "resolved"]:
+                    print(f"Updating market `{market['id']}`")
+                    dfq.at[index, "resolved"] = market["isResolved"]
+                    if not _entry_exists_for_today(dfmv[dfmv["id"] == market["id"]], utc_date_str):
+                        dfmv.loc[len(dfmv)] = _get_market_value_entry(
+                            market["id"], utc_datetime_str, market["probability"]
+                        )
 
     if new_markets:
         dfq = (
@@ -144,7 +169,7 @@ def driver(event, context):
     dfq, dfmv = _get_stored_question_data()
 
     # Get the latest Manifold data
-    response = _get_manifold_data()
+    response = _get_manifold_data(manifold_topic_slugs)
 
     # Update the existing questions
     dfq, dfmv = _update_questions(dfq, dfmv, response)
