1b49e13194bc3cdbb2305412f0013884eb223c4b
John Chen <john0922ycb@berkely.edu>
2024-04-24 15:13:15 -0700

    Yfinance
    close #3
    
    1. fetch S&P 500 stocks
    2. fetch missing prices, if weekends, then the price would be fridays' prices / it would only fetch the last date's price, if this is the first time fetching it (because we dont need the prices before the first time fetching it).
    3. If fetch stock prices during the weekend, it would fetch friday's price.
    
    add background

---
+++ src/questions/yfinance/fetch/main.py
@@ -0,0 +1,173 @@
+"""Yfinance fetch new questions script."""
+
+import json
+import logging
+import os
+import sys
+from datetime import timedelta
+
+import pandas as pd
+import requests
+import yfinance as yf
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../.."))
+from helpers import constants, data_utils, dates, decorator  # noqa: E402
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../.."))
+from utils import gcp  # noqa: E402
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+SOURCE = "yfinance"
+
+
+def get_sp500_tickers():
+    """
+    Retrieve the list of S&P 500 index constituent tickers.
+
+    Access the S&P 500 Wikipedia page and parse the HTML content to extract
+    the tickers of the constituents. Return a list of the tickers found
+    in the designated table on the page.
+
+    Returns:
+        list of str: A list containing the tickers of the S&P 500 index constituents.
+    """
+    try:
+        url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+        response = requests.get(url)
+        response.raise_for_status()  # Raises an HTTPError for bad responses
+        soup = BeautifulSoup(response.content, "html.parser")
+        table = soup.find("table", {"id": "constituents"})
+        tickers = [
+            row.find_all("td")[0].text.strip() for row in table.find_all("tr")[1:]
+        ]  # Skip header row
+        logger.info(f"Retrieved S&P 500 stock tickers: {tickers}")
+        return tickers
+    except Exception as e:
+        logger.error(f"Failed to retrieve stock tickers due to: {e}")
+        return []  # Return an empty list if there's any error
+
+
+def fetch_one_stock(ticker):
+    """
+    Fetch the company name and the latest historical stock data for a given stock ticker.
+
+    This function attempts to retrieve the company name and its most recent stock data for
+    the specified ticker using the yfinance library. It returns the company name and a
+    pandas DataFrame containing the historical stock data. If the ticker is invalid or
+    data retrieval fails, it returns None for both.
+
+    Parameters:
+    - ticker (str): The stock ticker for which to fetch the data.
+
+    Returns:
+    - tuple: A tuple containing the company name (str) and the historical stock data
+    (pandas.DataFrame) for the given ticker. Returns (None, None) if unable to fetch data.
+    """
+    try:
+        ticker = yf.Ticker(ticker)
+        company_name = ticker.info["longName"]
+        hist = ticker.history(period="2d").reset_index()
+        yesterday = dates.get_date_today() - timedelta(days=1)
+        hist["Date"] = pd.to_datetime(hist["Date"])
+        hist = hist[hist["Date"].dt.date <= yesterday].tail(1)
+        return company_name, hist
+
+    except Exception:
+        return None, None
+
+
+def fetch_all_stock(dfq):
+    """
+    Fetch and compile stock information for a list of stock tickers.
+
+    Iterates over a provided list of stock tickers, fetching the company name and the most
+    recent stock data for each. Constructs a detailed output string for each stock ticker,
+    along with additional metadata, and appends it to a list. This list is returned at the
+    end of the function. If data for a specific stock cannot be fetched, it is skipped.
+
+    Parameters:
+    - dfq (list of dict): A list of dicts for all stocks information in questions.jsonl.
+
+    Returns:
+    - list: A list of dictionaries. Each dictionary contains detailed information and
+    metadata about a stock, including its current price, if available.
+    """
+    stock_list = []
+
+    top_500_stocks = get_sp500_tickers()
+    current_stocks = dfq["id"].unique() if "id" in dfq.columns else []
+    set_top_500 = set(top_500_stocks)
+    set_current = set(current_stocks)
+    union_stocks_list = list(set_top_500.union(set_current))
+    logger.info(
+        f"Stock tickers not in top 500 but in current stocks: {set_current.difference(set_top_500)}"
+    )
+
+    for ticker in tqdm(union_stocks_list, desc="Fetching stock data"):
+        company_name, hist = fetch_one_stock(ticker)
+        current_time = dates.get_datetime_now()
+
+        if company_name and not hist.empty:
+            current_price = round(hist["Close"].iloc[-1], 2)
+            background = yf.Ticker(ticker).info["longBusinessSummary"]
+
+            stock_list.append(
+                {
+                    "id": ticker,
+                    "question": (
+                        f"Will {ticker}'s market close price at resolution be higher than "
+                        "its market close price on the forecast date?"
+                    ),
+                    "background": background,
+                    "source_resolution_criteria": "N/A",
+                    "source_begin_datetime": "N/A",
+                    "source_close_datetime": "N/A",
+                    "url": f"https://finance.yahoo.com/quote/{ticker}",
+                    "resolved": False,
+                    "source_resolution_datetime": "N/A",
+                    "fetch_datetime": current_time,
+                    "probability": current_price,
+                    "continual_resolution": True,
+                    "forecast_horizons": constants.FORECAST_HORIZONS_IN_DAYS,
+                    "value_at_freeze_datetime": current_price,
+                    "value_at_freeze_datetime_explanation": f"The latest market close price of {ticker}.",
+                }
+            )
+
+            logger.info(stock_list)
+
+    return pd.DataFrame(stock_list)
+
+
+@decorator.log_runtime
+def driver(_):
+    """Fetch all stock and then upload to gcp."""
+    dfq = data_utils.get_data_from_cloud_storage(SOURCE, return_question_data=True)
+
+    all_stock = fetch_all_stock(dfq)
+    filenames = data_utils.generate_filenames(SOURCE)
+
+    # Save and upload
+    with open(filenames["local_fetch"], "w", encoding="utf-8") as f:
+        # can't use `dfq.to_json` because we don't want escape chars
+        for record in all_stock.to_dict("records"):
+            json_str = json.dumps(record, ensure_ascii=False)
+            f.write(json_str + "\n")
+
+    logger.info("Uploading to GCP...")
+    # Upload
+    gcp.storage.upload(
+        bucket_name=constants.BUCKET_NAME,
+        local_filename=filenames["local_fetch"],
+    )
+    logger.info("Done.")
+
+    return "OK", 200
+
+
+if __name__ == "__main__":
+    driver(None)
---
+++ src/questions/yfinance/update_questions/main.py
@@ -0,0 +1,221 @@
+"""Yfinance update question script."""
+
+import logging
+import os
+import sys
+from datetime import timedelta
+
+import pandas as pd
+import yfinance as yf
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../.."))  # noqa: E402
+from helpers import constants, data_utils, dates, decorator  # noqa: E402
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../../.."))
+from utils import gcp  # noqa: E402
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+SOURCE = "yfinance"
+
+
+def fetch_one_stock(ticker, day_diff):
+    """
+    Fetch historical stock price data for a given ticker over a specified number of days.
+
+    Retrieve the closing prices of a stock from the Yahoo Finance API, spanning the number of days
+    specified by 'day_diff' counting backwards from today. The function handles any exceptions
+    during the data retrieval process and returns an empty DataFrame if the data fetch fails.
+
+    Parameters:
+    - ticker (str): The stock symbol for which to retrieve price data.
+    - day_diff (int): The number of days from today for which to retrieve historical data.
+
+    Returns:
+    - DataFrame: A pandas DataFrame containing the historical closing prices of the stock with
+      columns 'date' and 'value', where 'date' is the date of the closing price and
+      'value' is the closing price itself. If the data fetch fails, returns an empty DataFrame.
+    """
+    try:
+        ticker = yf.Ticker(ticker)
+        hist = ticker.history(period=f"{day_diff}d")
+        return hist[["Close"]].reset_index().rename(columns={"Date": "date", "Close": "value"})
+    except Exception as e:
+        print(f"Failed to fetch data for {ticker}: {e}")
+        return pd.DataFrame()
+
+
+def get_historical_prices(current_df, ticker):
+    """
+    Update a DataFrame with the latest historical stock prices for a given ticker.
+
+    Determine the period to fetch based on the most recent date in the provided DataFrame and fill
+    any missing days, including weekends and holidays, by carrying forward the last available price.
+    If the input DataFrame is empty, initialize it and fetch data for at least the last day. The
+    resulting DataFrame includes a complete series of dates and corresponding stock prices,
+    indexed daily from the earliest date available in the input or from the last day if the DataFrame
+    was initially empty.
+
+    Parameters:
+    - current_df (DataFrame): A pandas DataFrame containing columns ['id', 'date', 'value'],
+      where 'id' is the stock ticker, 'date' is the date, and 'value' is the stock price.
+    - ticker (str): The stock symbol for which to retrieve and update prices.
+
+    Returns:
+    - DataFrame: A pandas DataFrame updated with daily historical stock prices from the most recent
+      date in 'current_df' to the present day. The DataFrame is sorted by 'date' and includes
+      the columns ['id', 'date', 'value'].
+    """
+    current_time = dates.get_date_today()
+    yesterday = current_time - timedelta(days=1)
+    if current_df.empty:
+        # If the dataframe is empty, only fetch last date's stock price
+        current_df = pd.DataFrame(columns=["id", "date", "value"])
+        hist_data = fetch_one_stock(ticker, 5).reset_index()
+        hist_data["date"] = pd.to_datetime(hist_data["date"])
+        hist_data = hist_data[hist_data["date"].dt.date <= yesterday].tail(1)
+    else:
+        # If not empty, fetch all stock prices up until yesterday's
+        last_date = pd.to_datetime(current_df["date"]).max().date()
+        day_diff = (current_time - last_date).days
+        hist_data = fetch_one_stock(ticker, day_diff)
+
+    hist_data["date"] = pd.to_datetime(hist_data["date"]).dt.date
+    if not current_df.empty:
+        current_df["date"] = pd.to_datetime(current_df["date"]).dt.date
+
+    # Check if there is any data to process
+    if not current_df.empty or (not hist_data.empty and hist_data["date"].iloc[0] < yesterday):
+        # Concatenate and handle duplicates based on 'date' only if there's current data to add
+        if not current_df.empty:
+            all_data = pd.concat([current_df, hist_data])
+        else:
+            all_data = hist_data.copy()
+
+        # Sort and remove duplicates to keep the latest entry per date
+        all_data.sort_values(by="date", inplace=True)
+        all_data.drop_duplicates(subset=["date"], keep="last", inplace=True)
+
+        # Reindex to fill in missing dates including weekends
+        all_dates = pd.date_range(start=all_data["date"].min(), end=yesterday, freq="D")
+        all_data = all_data.set_index("date").reindex(all_dates, method="ffill").reset_index()
+
+        # Ensure the reset index is named 'date' and convert to datetime.date if not already
+        if current_df.empty:
+            all_data.rename(columns={"level_0": "date"}, inplace=True)
+        else:
+            all_data.rename(columns={"index": "date"}, inplace=True)
+            all_data["date"] = all_data["date"].dt.date
+    else:
+        all_data = hist_data.copy()
+
+    all_data["id"] = ticker
+    all_data.drop_duplicates(subset=["date"], keep="last", inplace=True)
+
+    return all_data[["id", "date", "value"]].astype(dtype=constants.RESOLUTION_FILE_COLUMN_DTYPE)
+
+
+def create_resolution_file(question, get_historical_forecasts_func, source):
+    """
+    Create or update a resolution file based on the question ID provided. Download the existing file, if any.
+
+    Check the last entry date, and update with new data if there's no entry for today. Upload the updated file
+    back to the specified Google Cloud Platform bucket.
+    Args:
+    - question (dict): A dictionary containing at least the 'id' of the question.
+    - get_historical_forecasts_func (function, optional): A function to retrieve historical forecasts.
+      Defaults to `get_historical_prices`.
+    - source (str): The source directory path within the bucket where files are stored.
+    Returns:
+    - DataFrame: Return the current state of the resolution file as a DataFrame if no update is needed.
+      If an update occurs, the function returns None after uploading the updated file.
+    """
+    basename = f"{question['id']}.jsonl"
+    remote_filename = f"{source}/{basename}"
+    local_filename = "/tmp/tmp.jsonl"
+
+    gcp.storage.download_no_error_message_on_404(
+        bucket_name=constants.BUCKET_NAME,
+        filename=remote_filename,
+        local_filename=local_filename,
+    )
+    df = pd.read_json(
+        local_filename,
+        lines=True,
+        dtype=constants.RESOLUTION_FILE_COLUMN_DTYPE,
+        convert_dates=False,
+    )
+
+    yesterday = dates.get_date_today() - timedelta(days=1)
+
+    if not df.empty and pd.to_datetime(df["date"].iloc[-1]).date() >= yesterday:
+        logger.info(f"{question['id']} is skipped because it's already up-to-date!")
+        # Check last date to see if we've already gotten the resolution value for today
+        # If we have it alreeady, return to avoid unnecessary API calls
+        return df
+
+    df = get_historical_forecasts_func(df, question["id"])
+
+    df.to_json(local_filename, orient="records", lines=True, date_format="iso")
+    gcp.storage.upload(
+        bucket_name=constants.BUCKET_NAME,
+        local_filename=local_filename,
+        filename=remote_filename,
+    )
+
+
+def update_questions(dfq, dff):
+    """
+    Update the dataframes with new or modified question data and new community predictions.
+
+    Parameters:
+    - dfq (pd.DataFrame): DataFrame containing all existing questions.
+    - dff  (pd.DataFrame): DataFrame containing all newly fetched questions.
+
+    The function updates dfq by either replacing existing questions with new data or adding new questions.
+    It also appends new community predictions to dfr for each question in all_questions_to_add.
+    """
+    dff_list = dff.to_dict("records")
+    for question in dff_list:
+        create_resolution_file(question, get_historical_prices, SOURCE)
+
+        del question["fetch_datetime"]
+        del question["probability"]
+
+        # Check if the question exists in dfq
+        if question["id"] in dfq["id"].values:
+            # Case 1: Update existing question
+            dfq_index = dfq.index[dfq["id"] == question["id"]].tolist()[0]
+            for key, value in question.items():
+                dfq.at[dfq_index, key] = value
+        else:
+            # Case 2: Append new question
+            new_q_row = pd.DataFrame([question])
+            new_q_row = new_q_row.astype(constants.QUESTION_FILE_COLUMN_DTYPE)
+            dfq = pd.concat([dfq, new_q_row], ignore_index=True)
+
+    return dfq
+
+
+@decorator.log_runtime
+def driver(_):
+    """Execute the main workflow of fetching, processing, and uploading questions."""
+    # Download existing questions from cloud storage
+    dfq, dff = data_utils.get_data_from_cloud_storage(
+        SOURCE, return_question_data=True, return_fetch_data=True
+    )
+
+    # Update the existing questions
+    dfq = update_questions(dfq, dff)
+
+    logger.info("Uploading to GCP...")
+    # Save and upload
+    data_utils.upload_questions(dfq, SOURCE)
+    logger.info("Done.")
+
+    return "OK", 200
+
+
+if __name__ == "__main__":
+    driver(None)
