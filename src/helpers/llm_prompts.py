d79419240a67bb09412573209eac1949ff07bafe
John Chen <john0922ycb@berkely.edu>
2024-05-27 19:37:39 -0700

    LLM_Crowd
    close #60

--- src/helpers/llm_prompts.py
+++ src/helpers/llm_prompts.py
@@ -7,7 +7,7 @@ HUMAN_JOINT_PROMPT_2 = "Below, you'll see two probability questions. We're going
 HUMAN_JOINT_PROMPT_3 = "Below, you'll see two probability questions. We're going to ask you to predict the probability that question 2 will happen, but not question 1."  # noqa: B950
 HUMAN_JOINT_PROMPT_4 = "Below, you'll see two probability questions. We're going to ask you to predict the probability that neither will happen."  # noqa: B950
 
-ZERO_SHOT_PROMPT = """
+ZERO_SHOT_MARKET_PROMPT = """
 You are an expert superforecaster, familiar with the work of Tetlock and others. Make a prediction of the probability that the question will be resolved as true. You MUST give a probability estimate between 0 and 1 UNDER ALL CIRCUMSTANCES. If for some reason you can't answer, pick the base rate, but return a number between 0 and 1.
 
 Question:
@@ -26,13 +26,10 @@ Do not output anything else.
 Answer: {{ Insert answer here }}
 """  # noqa: B950
 
-ZERO_SHOT_ACLED_PROMPT = """
+ZERO_SHOT_NON_MARKET_PROMPT = """
 You are an expert superforecaster, familiar with the work of Tetlock and others. Make a prediction of the probability that the question will be resolved as true. You MUST give a probability estimate between 0 and 1 UNDER ALL CIRCUMSTANCES. If for some reason you can't answer, pick the base rate, but return a number between 0 and 1.
 
-The Armed Conflict Location & Event Data Project (ACLED) collects real-time data on the locations, dates, actors, fatalities, and types of all reported political violence and protest events around the world.
-
-You’re going to predict the probability of the following potential outcome we’ve come up with about some of the data ACLED tracks “at each of the resolution dates”.
-
+You’re going to predict the probability of the following potential outcome “at each of the resolution dates”.
 
 Question:
 {question}
@@ -99,7 +96,7 @@ Do not output anything else.
 Answer: {{ Insert answer here }}
 """  # noqa: B950
 
-SCRATCH_PAD_PROMPT = """
+SCRATCH_PAD_MARKET_PROMPT = """
 Question:
 {question}
 
@@ -134,10 +131,8 @@ Instructions:
 {{ Insert your answer }}
 """  # noqa: B950
 
-SCRATCH_PAD_ACLED_PROMPT = """
-The Armed Conflict Location & Event Data Project (ACLED) collects real-time data on the locations, dates, actors, fatalities, and types of all reported political violence and protest events around the world.
-
-You’re going to predict the probability of the following potential outcome we’ve come up with about some of the data ACLED tracks “at each of the resolution dates”.
+SCRATCH_PAD_NON_MARKET_PROMPT = """
+You’re going to predict the probability of the following potential outcome “at each of the resolution dates”.
 
 Question:
 {question}
@@ -147,6 +142,12 @@ Question Background:
 
 Resolution Criteria:
 {resolution_criteria}
+
+Current value on {freeze_datetime}:
+{value_at_freeze_datetime}
+
+Value Explanation:
+{value_at_freeze_datetime_explanation}
 
 Question resolution date: {list_of_resolution_dates}
 
@@ -174,8 +175,6 @@ Instructions:
 """  # noqa: B950
 
 SCRATCH_PAD_JOINT_QUESTION_PROMPT = """
-The Armed Conflict Location & Event Data Project (ACLED) collects real-time data on the locations, dates, actors, fatalities, and types of all reported political violence and protest events around the world.
-
 {human_prompt} “at each of the resolution dates”.
 
 Question 1:
@@ -233,6 +232,154 @@ Instructions:
 {{ Insert your answer }}
 """  # noqa: B950
 
+SCRATCH_PAD_WITH_SUMMARIES_MARKET_PROMPT = """
+Question:
+{question}
+
+Question Background:
+{background}
+
+Resolution Criteria:
+{resolution_criteria}
+
+Question close date: {close_date}
+
+We have retrieved the following information for this question:
+{retrieved_info}
+
+Instructions:
+1. Given the above question, rephrase and expand it to help you do better answering. Maintain all information in the original question.
+{{ Insert rephrased and expanded question.}}
+
+2. Provide a few reasons why the answer might be no. Rate the strength of each reason.
+{{ Insert your thoughts }}
+
+3. Provide a few reasons why the answer might be yes. Rate the strength of each reason.
+{{ Insert your thoughts }}
+
+4. Aggregate your considerations. Think like a superforecaster (e.g. Nate Silver).
+{{ Insert your aggregated considerations }}
+
+5. Output an initial probability (prediction) given steps 1-4.
+{{ Insert initial probability. }}
+
+6. Evaluate whether your calculated probability is excessively confident or not confident enough. Also, consider anything else that might affect the forecast that you did not before consider.
+{{ Insert your thoughts }}
+
+7. Output your answer (a number between 0 and 1) with an asterisk at the beginning and end of the decimal. (For example, if there are n resolution dates, you would output different *p* for each resolution date) Do not output anything else.
+{{ Insert your answer }}
+"""  # noqa: B950
+
+SCRATCH_PAD_WITH_SUMMARIES_NON_MARKET_PROMPT = """
+You’re going to predict the probability of the following potential outcome “at each of the resolution dates”.
+
+Question:
+{question}
+
+Question Background:
+{background}
+
+Resolution Criteria:
+{resolution_criteria}
+
+Current value on {freeze_datetime}:
+{value_at_freeze_datetime}
+
+Value Explanation:
+{value_at_freeze_datetime_explanation}
+
+We have retrieved the following information for this question:
+{retrieved_info}
+
+Question resolution date: {list_of_resolution_dates}
+
+Instructions:
+1. Given the above question, rephrase and expand it to help you do better answering. Maintain all information in the original question.
+{{ Insert rephrased and expanded question.}}
+
+2. Provide a few reasons why the answer might be no. Rate the strength of each reason.
+{{ Insert your thoughts }}
+
+3. Provide a few reasons why the answer might be yes. Rate the strength of each reason.
+{{ Insert your thoughts }}
+
+4. Aggregate your considerations. Think like a superforecaster (e.g. Nate Silver).
+{{ Insert your aggregated considerations }}
+
+5. Output an initial probability (prediction) given steps 1-4.
+{{ Insert initial probability. }}
+
+6. Evaluate whether your calculated probability is excessively confident or not confident enough. Also, consider anything else that might affect the forecast that you did not before consider.
+{{ Insert your thoughts }}
+
+7. Output your answer (a number between 0 and 1) with an asterisk at the beginning and end of the decimal. (For example, if there are n resolution dates, you would output different *p* for each resolution date) Do not output anything else.
+{{ Insert your answer }}
+"""  # noqa: B950
+
+SCRATCH_PAD_WITH_SUMMARIES_JOINT_QUESTION_PROMPT = """
+{human_prompt} “at each of the resolution dates”.
+
+Question 1:
+{question_1}
+
+Question 2:
+{question_2}
+
+Question 1 Background:
+{background_1}
+
+Question 2 Background:
+{background_2}
+
+Question 1 Resolution Criteria:
+{resolution_criteria_1}
+
+Question 2 Resolution Criteria:
+{resolution_criteria_2}
+
+Question 1 Current value on {freeze_datetime_1}:
+{value_at_freeze_datetime_1}
+
+Question 1 Value Explanation:
+{value_at_freeze_datetime_explanation_1}
+
+Question 2 Current value on {freeze_datetime_1}:
+{value_at_freeze_datetime_2}
+
+Question 2 Value Explanation:
+{value_at_freeze_datetime_explanation_2}
+
+We have retrieved the following information for Question 1:
+{retrieved_info_1}
+
+We have retrieved the following information for Question 2:
+{retrieved_info_2}
+
+Question resolution date: {list_of_resolution_dates}
+
+Instructions:
+1. Given the above question, rephrase and expand it to help you do better answering. Maintain all information in the original question.
+{{ Insert rephrased and expanded question.}}
+
+2. Provide a few reasons why the answer might be no. Rate the strength of each reason.
+{{ Insert your thoughts }}
+
+3. Provide a few reasons why the answer might be yes. Rate the strength of each reason.
+{{ Insert your thoughts }}
+
+4. Aggregate your considerations. Think like a superforecaster (e.g. Nate Silver).
+{{ Insert your aggregated considerations }}
+
+5. Output an initial probability (prediction) given steps 1-4.
+{{ Insert initial probability. }}
+
+6. Evaluate whether your calculated probability is excessively confident or not confident enough. Also, consider anything else that might affect the forecast that you did not before consider.
+{{ Insert your thoughts }}
+
+7. Output your answer (a number between 0 and 1) with an asterisk at the beginning and end of the decimal. (For example, if there are n resolution dates, you would output different *p* for each resolution date) Do not output anything else.
+{{ Insert your answer }}
+"""  # noqa: B950
+
 REFORMAT_PROMPT = """
 User prompt:
 {user_prompt}
@@ -240,10 +387,21 @@ User prompt:
 Model Response:
 {model_response}
 
-Please determine whether the model's response includes a probabilistic forecast for all {n_horizons} resolution dates mentioned in the user's prompt. If such forecasts are not present, you should provide a probabilistic forecast for all {n_horizons} resolution dates mentioned.
-The overarching question is: 'Below, you'll find two probability questions. We'd like you to predict the probability that neither event will occur at each of the resolution dates.'
+Please determine the model's final probabilistic forecasts for all {n_horizons} resolution dates mentioned in the user's prompt. If such forecasts are not present, you should provide a probabilistic forecast for all {n_horizons} resolution dates mentioned.
 
 Please output the probabilistic forecasts as a Python list, e.g., [prob1, prob2, ...] DO NOT OUTPUT ANYTHING ELSE. PLEASE ONLY OUTPUT [prob1, prob2, ...], if there is only one probabilistic prediction, output [prob1, prob2, ...] with the same probability.
+"""  # noqa: B950
+
+REFORMAT_PROMPT_2 = """
+User prompt:
+{user_prompt}
+
+Model Response:
+{model_response}
+
+Please determine the model's final probabilistic forecasts for all {n_horizons} resolution dates mentioned in the user's prompt. If any forecasts are missing, provide a probabilistic forecast for each of the resolution dates.
+
+Output the probabilistic forecasts as a Python list, e.g., [prob1, prob2, ...]. DO NOT OUTPUT ANYTHING ELSE. If there is only one probabilistic prediction, replicate it for all dates.
 """  # noqa: B950
 
 REFORMAT_SINGLE_PROMPT = """
@@ -257,6 +415,18 @@ Task: Extract the numerical probability given by the LLM. The probability should
 If the LLM's response does not contain a probability estimate, return 'N/A'.
 
 Output Requirement: Provide only a decimal value between 0 and 1 representing the probability, or 'N/A' if no probability is mentioned.
+"""  # noqa: B950
+
+REFORMAT_SINGLE_PROMPT_2 = """
+{response}
+
+***********************************
+Instructions:
+The text above is a response from a large language model (LLM) that includes both reasoning and a probability estimate in answer to a question.
+
+Task: Extract the numerical probability mentioned by the LLM. This probability should be a decimal value between 0 and 1.
+
+Output Requirement: Provide only the extracted probability as a decimal value between 0 and 1. If the response does not contain a probability estimate, return 'N/A'.
 """  # noqa: B950
 
 
