670b1ad4157603dc184ef16e46ac15014323d768
Houtan Bastani <houtan@forecastingresearch.org>
2024-09-16 21:17:46 +0200

    leaderboard: update
    - changes for various updates across system
    - display imputed %
    - equal weight data/market questions in overall score
    - resolve all unresolved market questions across all forecast horizons to the latest market value
    - only count each market questions once (as opposed to once/horizon) in tables

--- src/leaderboard/main.py
+++ src/leaderboard/main.py
@@ -10,6 +10,7 @@ import pandas as pd
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from helpers import (  # noqa: E402
     constants,
+    dates,
     decorator,
     env,
     question_curation,
@@ -49,29 +50,18 @@ def upload_leaderboard(df, basename):
     )
 
 
-def download_human_question_set(forecast_date):
-    """Download the question set that was given to humans."""
-    df = pd.read_json(
-        f"gs://{env.QUESTION_SETS_BUCKET}/{forecast_date}-human.jsonl",
-        lines=True,
-        convert_dates=False,
-    )
-    df = resolution.make_columns_hashable(df)
-    # DROP COMBO QUESTIONS FOR MARKETS
-    df = df[
-        ~df.apply(
-            lambda x: resolution.is_combo(x) and x["source"] not in question_curation.DATA_SOURCES,
-            axis=1,
-        )
-    ].reset_index(drop=True)
-    return df
-
-
 def get_leaderboard_entry(df):
     """Create the leaderboard entry for the given dataframe."""
     # Masks
     data_mask = df["source"].isin(question_curation.DATA_SOURCES)
-    market_mask = ~data_mask
+
+    # Market sources should be reduced to the value at a single date. This is because we always
+    # evaluate to the latest market value or the resolution value for a market and orgs only
+    # forecast the outcome. Hence they get the same score at every period.
+    single_resolution_date = sorted(df["resolution_date"].unique())[0]
+    market_mask = df["source"].isin(question_curation.MARKET_SOURCES) & (
+        df["resolution_date"] == single_resolution_date
+    )
 
     resolved_mask = df["resolved"].astype(bool)
     unresolved_mask = ~resolved_mask
@@ -89,9 +79,12 @@ def get_leaderboard_entry(df):
     market_overall_score, n_market_overall = get_scores(df, market_mask)
 
     # Overall
-    overall_score = df["score"].mean()
-    overall_std_dev = df["score"].std()
+    overall_score = (data_resolved_score + market_overall_score) / 2
+    overall_std_dev = df["score"].mean()  # DELETE
     n_overall = len(df)
+
+    # % imputed
+    pct_imputed = int(df["imputed"].sum() / len(df) * 100)
 
     return {
         "data": data_resolved_score,
@@ -104,17 +97,23 @@ def get_leaderboard_entry(df):
         "n_market_overall": n_market_overall,
         "overall": overall_score,
         "n_overall": n_overall,
+        "pct_imputed": pct_imputed,
         "std_dev": overall_std_dev,
     }
 
 
-def add_to_leaderboard(leaderboard, org_and_model, df):
+def add_to_leaderboard(leaderboard, org_and_model, df, forecast_due_date):
     """Add scores to the leaderboard."""
-    horizons = df["horizon"].unique()
-    for horizon in horizons:
-        horizon_key = str(int(horizon))
-        leaderboard_entry = [org_and_model | get_leaderboard_entry(df[df["horizon"] == horizon])]
-        leaderboard[horizon_key] = leaderboard.get(horizon_key, []) + leaderboard_entry
+    resolution_dates = df["resolution_date"].unique()
+    forecast_due_date_date = dates.convert_iso_str_to_date(forecast_due_date)
+    for resolution_date in resolution_dates:
+        resolution_date_key = (resolution_date - forecast_due_date_date).days
+        leaderboard_entry = [
+            org_and_model | get_leaderboard_entry(df[df["resolution_date"] == resolution_date])
+        ]
+        leaderboard[resolution_date_key] = (
+            leaderboard.get(resolution_date_key, []) + leaderboard_entry
+        )
 
     leaderboard_entry = [org_and_model | get_leaderboard_entry(df)]
     leaderboard["overall"] = leaderboard.get("overall", []) + leaderboard_entry
@@ -127,23 +126,32 @@ def add_to_llm_leaderboard(*args, **kwargs):
     return add_to_leaderboard(*args, **kwargs)
 
 
-def add_to_llm_and_human_leaderboard(leaderboard, org_and_model, df, forecast_date):
+def add_to_llm_and_human_leaderboard(leaderboard, org_and_model, df, forecast_due_date):
     """Parse the forecasts to include only those questions that were in the human question set."""
-    df_human_question_set = download_human_question_set(forecast_date)
-    df_only_human_question_set = pd.merge(
-        df, df_human_question_set[["id", "source"]], on=["id", "source"], how="inner"
+    df_human_question_set = resolution.download_and_read_question_set_file(
+        filename=f"{forecast_due_date}-human.json"
     )
+    df_only_human_question_set = pd.merge(
+        df,
+        df_human_question_set[["id", "source"]],
+        on=["id", "source"],
+    ).reset_index(drop=True)
     return add_to_leaderboard(
-        leaderboard=leaderboard, org_and_model=org_and_model, df=df_only_human_question_set
+        leaderboard=leaderboard,
+        org_and_model=org_and_model,
+        df=df_only_human_question_set,
+        forecast_due_date=forecast_due_date,
     )
 
 
 def make_html_table(df, title, basename):
     """Make and upload HTLM leaderboard."""
     # Replace NaN with empty strings for display
+    logger.info(f"Maknig leaderboard table: {title} {basename}.")
     df = df.fillna("")
 
     # Add ranking
+    df = df.sort_values(by=["overall"], ignore_index=True)
     df.insert(loc=0, column="Ranking", value="")
     df["score_diff"] = df["overall"] - df["overall"].shift(1)
     for index, row in df.iterrows():
@@ -162,6 +170,9 @@ def make_html_table(df, title, basename):
     n_market_unresolved = df["n_market_unresolved"].max()
     n_market_overall = df["n_market_overall"].max()
     n_overall = df["n_overall"].max()
+
+    df["pct_imputed"] = df["pct_imputed"].astype(str) + "%"
+
     df = df.rename(
         columns={
             "organization": "Organization",
@@ -171,8 +182,9 @@ def make_html_table(df, title, basename):
             "market_unresolved": f"Market Score (unresolved) (N={n_market_unresolved:,})",
             "market_overall": f"Market Score (overall) (N={n_market_overall:,})",
             "overall": f"Overall Score (N={n_overall:,})",
-            "std_dev": "Std. Dev.",
-            "z_score_wrt_naive_mean": "Z-score",
+            "pct_imputed": "Pct. Imputed",
+            "std_dev": "Old Mean",  # Std. Dev.", # DELETE
+            #            "z_score_wrt_naive_mean": "Z-score",
         }
     )
 
@@ -189,13 +201,14 @@ def make_html_table(df, title, basename):
     <head>
         <meta charset="UTF-8">
         <title>LLM Data Table</title>
-        <link rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+              integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+              crossorigin="anonymous">
         <link rel="stylesheet" type="text/css"
-          href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
-        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+              href="https://cdn.datatables.net/2.1.6/css/dataTables.jqueryui.min.css">
+        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
         <script type="text/javascript" charset="utf8"
-          src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
+                src="https://cdn.datatables.net/2.1.6/js/dataTables.min.js"></script>
         <style>
             body {
               font-size: 10px;
@@ -280,47 +293,55 @@ def driver(_):
     llm_and_human_leaderboard = {}
     files = gcp.storage.list(env.PROCESSED_FORECAST_SETS_BUCKET)
     files = [file for file in files if file.endswith(".json")]
+    # files = files[0:2]  # DELETE THIS
     for f in files:
         logger.info(f"Downloading, reading, and scoring forecasts in `{f}`...")
 
         data = download_and_read_forecast_file(filename=f)
         if not data or not isinstance(data, dict):
+            logger.warning(f"Problem processing {f}. First `continue`.")
             continue
 
         organization = data.get("organization")
         model = data.get("model")
         question_set_filename = data.get("question_set")
-        forecast_date = data.get("forecast_date")
+        forecast_due_date = data.get("forecast_due_date")
         forecasts = data.get("forecasts")
         if (
             not organization
             or not model
             or not question_set_filename
-            or not forecast_date
+            or not forecast_due_date
             or not forecasts
         ):
+            logger.warning(f"Problem processing {f}. Second `continue`.")
             continue
 
         df = pd.DataFrame(forecasts)
         if df.empty:
+            logger.warning(f"Problem processing {f}. Third `continue`.")
             continue
         df = resolution.make_columns_hashable(df)
+        df["resolution_date"] = pd.to_datetime(df["resolution_date"]).dt.date
+        df["forecast_due_date"] = pd.to_datetime(df["forecast_due_date"]).dt.date
 
         is_human_question_set = "human" in question_set_filename
         org_and_model = {"organization": organization, "model": model}
         if not is_human_question_set:
-            llm_leaderboard = add_to_llm_leaderboard(llm_leaderboard, org_and_model, df)
+            llm_leaderboard = add_to_llm_leaderboard(
+                llm_leaderboard, org_and_model, df, forecast_due_date
+            )
         llm_and_human_leaderboard = add_to_llm_and_human_leaderboard(
-            llm_and_human_leaderboard, org_and_model, df, forecast_date
+            llm_and_human_leaderboard, org_and_model, df, forecast_due_date
         )
 
     def get_z_score(df):
-        mask = (df["organization"] == BASELINE_ORG_MODEL["organization"]) & (
-            df["model"] == BASELINE_ORG_MODEL["model"]
-        )
-        naive_baseline_mean = df[mask]["overall"].values[0]
-        naive_std_dev = df[mask]["std_dev"].values[0]
-        df["z_score_wrt_naive_mean"] = (df["overall"] - naive_baseline_mean) / naive_std_dev
+        # mask = (df["organization"] == BASELINE_ORG_MODEL["organization"]) & (
+        #     df["model"] == BASELINE_ORG_MODEL["model"]
+        # )
+        # naive_baseline_mean = df[mask]["overall"].values[0]
+        # naive_std_dev = df[mask]["std_dev"].values[0]
+        # df["z_score_wrt_naive_mean"] = (df["overall"] - naive_baseline_mean) / naive_std_dev
         return df
 
     def is_numeric(s):
@@ -332,21 +353,25 @@ def driver(_):
 
     for key in llm_leaderboard:
         title = "Leaderboard: " + (f"{key} day" if is_numeric(key) else "overall")
-        df_llm = get_z_score(pd.DataFrame(llm_leaderboard[key])).sort_values(
-            by=["overall", "z_score_wrt_naive_mean"], ignore_index=True
-        )
-        upload_leaderboard(df=df_llm, basename=f"leaderboard_lm_{key}")
-        make_html_table(df=df_llm, title=title, basename=f"lm_leaderboard_{key}")
+        df_llm = pd.DataFrame(llm_leaderboard[key])
+        # df_llm = get_z_score(df_llm).sort_values(
+        #     by=["overall", "z_score_wrt_naive_mean"], ignore_index=True
+        # )
+
+        upload_leaderboard(df=df_llm, basename=f"leaderboard_{key}")
+        make_html_table(df=df_llm, title=title, basename=f"leaderboard_{key}")
 
         if key in llm_and_human_leaderboard:
-            df_llm_human = get_z_score(pd.DataFrame(llm_and_human_leaderboard[key])).sort_values(
-                by=["overall", "z_score_wrt_naive_mean"], ignore_index=True
-            )
-            upload_leaderboard(df=df_llm_human, basename=f"leaderboard_lm_human_{key}")
+            df_llm_human = pd.DataFrame(llm_and_human_leaderboard[key])
+            # df_llm_human = get_z_score(df_llm_human).sort_values(
+            #     by=["overall", "z_score_wrt_naive_mean"], ignore_index=True
+            # )
+
+            upload_leaderboard(df=df_llm_human, basename=f"human_leaderboard_{key}")
             make_html_table(
                 df=df_llm_human,
                 title=f"Human {title}",
-                basename=f"lm_human_leaderboard_{key}",
+                basename=f"human_leaderboard_{key}",
             )
 
     logger.info("Done.")
