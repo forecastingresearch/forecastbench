forecastingresearch/forecastbench

commit d25f0cd5c865277a26ea12c0c2768fe3829888f5

orchestration: increase timeouts when generating llm baselines

src/nightly_update_workflow/manager/main.py
@@ -26,8 +26,11 @@ def call_worker(dict_to_use, task_count):
 def main():
     """Manage nightly run."""
     dict_to_use_publish_question_set = "publish_question_set_make_llm_baseline"
+    timeout_publish_question_set = cloud_run.timeout_1h * 8
     operation_publish_question_set = call_worker(
-        dict_to_use=dict_to_use_publish_question_set, task_count=1
+        dict_to_use=dict_to_use_publish_question_set,
+        task_count=1,
+        timeout=timeout_publish_question_set,
     )
 
     dict_to_use = "fetch_and_update"
@@ -71,23 +74,24 @@ def main():
         exit_on_error=True,
     )
 
-    cloud_run.block_and_check_job_result(
-        operation=operation_publish_question_set,
-        name=dict_to_use_publish_question_set,
-        exit_on_error=True,
-    )
-
-    cloud_run.block_and_check_job_result(
-        operation=operation_naive_forecaster,
-        name=dict_to_use_naive_forecaster,
-        exit_on_error=True,
-    )
-
     dict_to_use_website = "website"
     operation_website = call_worker(dict_to_use=dict_to_use_website, task_count=1)
     cloud_run.block_and_check_job_result(
         operation=operation_website,
         name=dict_to_use_website,
+        exit_on_error=True,
+    )
+
+    cloud_run.block_and_check_job_result(
+        operation=operation_publish_question_set,
+        name=dict_to_use_publish_question_set,
+        exit_on_error=True,
+        timeout=timeout_publish_question_set,
+    )
+
+    cloud_run.block_and_check_job_result(
+        operation=operation_naive_forecaster,
+        name=dict_to_use_naive_forecaster,
         exit_on_error=True,
     )
 

src/base_eval/llm_baselines/manager/main.py
@@ -47,6 +47,7 @@ def main():
 
     logger.info(f"Running LLM baselines for: {forecast_due_date}-llm.json")
 
+    timeout = cloud_run.timeout_1h * 8
     operation = cloud_run.call_worker(
         job_name="func-baseline-llm-forecasts-worker",
         env_vars={
@@ -54,11 +55,13 @@ def main():
             "TEST_OR_PROD": args.mode,
         },
         task_count=9,
+        timeout=timeout,
     )
     cloud_run.block_and_check_job_result(
         operation=operation,
         name="llm-baselines",
         exit_on_error=True,
+        timeout=timeout,
     )
 
 

src/base_eval/llm_baselines/manager/Makefile
@@ -28,7 +28,7 @@ deploy : main.py .gcloudignore requirements.txt Dockerfile
 		--region $(CLOUD_DEPLOY_REGION) \
 		--tasks 1 \
 		--parallelism 1 \
-		--task-timeout 3h \
+		--task-timeout 8h \
 		--memory 2Gi \
 		--max-retries 0 \
 		--service-account $(WORKFLOW_SERVICE_ACCOUNT) \

src/helpers/cloud_run.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 timeout_1h = 3600
 
 
-def call_worker(job_name, env_vars, task_count):
+def call_worker(job_name, env_vars, task_count, timeout=timeout_1h):
     """Invoke a Cloud Run Job.
 
     Params:
@@ -32,6 +32,7 @@ def call_worker(job_name, env_vars, task_count):
         job_name=job_name,
         env_vars=env_vars,
         task_count=task_count,
+        timeout=timeout,
     )
 
 

src/nightly_update_workflow/manager/Makefile
@@ -19,7 +19,7 @@ deploy : main.py requirements.txt  Procfile
 		--region $(CLOUD_DEPLOY_REGION) \
 		--tasks 1 \
 		--parallelism 1 \
-		--task-timeout 4h \
+		--task-timeout 8h \
 		--memory 512Mi \
 		--max-retries 0 \
 		--service-account $(WORKFLOW_SERVICE_ACCOUNT) \

commit 76983fd663ce2d75f92e11bcd53052adf69c833f

orchestration: fix bug introduced in d25f0cd

src/nightly_update_workflow/manager/main.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def call_worker(dict_to_use, task_count):
+def call_worker(dict_to_use, task_count, timeout=cloud_run.timeout_1h):
     """Make main() easier to read."""
     return cloud_run.call_worker(
         job_name="nightly-worker",
@@ -20,6 +20,7 @@ def call_worker(dict_to_use, task_count):
             "DICT_TO_USE": dict_to_use,
         },
         task_count=task_count,
+        timeout=timeout,
     )
 
 