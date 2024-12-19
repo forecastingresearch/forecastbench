forecastingresearch/forecastbench

commit f6d23875cacc29dfbae75cd321b95bf5a57ad330

resolution: mark combo questions resolved if one question has resolved in the opposite direction
If a question resolves Yes then all combo questions where the user was asked to forecast on an
outcome in the direction of -1 have resolved since, no matter how the other question resolves, the
combination of the two questions has resolved to 0.

src/helpers/resolution.py
@@ -58,6 +58,32 @@ def is_combo(row):
     raise ValueError(f"Problem in `is_combo` with {row}. This type is not handled: {type(row)}")
 
 
+def _is_combo_question_resolved_helper(is_resolved, direction, resolution):
+    """Determine whether or not a combo question has resolved for a single direction."""
+    if is_resolved and ((direction == 1 and not resolution) or (direction == -1 and resolution)):
+        return True
+    return False
+
+
+def is_combo_question_resolved(is_resolved0, is_resolved1, dir0, dir1, resolution0, resolution1):
+    """Determine whether or not a combo question has resolved.
+
+    Combo questions are asked in 4 directions: (1,1), (1,-1), (-1,1), (-1,-1). If q2 has resolved,
+    then 2 of the directions have resolved as well. e.g. if q2 resolves No, then questions with
+    directions (1,1) and (-1,1) have resolved to 0. No matter the outcome of q1, the score for these
+    two questions will not change.
+    """
+    return _is_combo_question_resolved_helper(
+        is_resolved=is_resolved0,
+        direction=dir0,
+        resolution=resolution0,
+    ) or _is_combo_question_resolved_helper(
+        is_resolved=is_resolved1,
+        direction=dir1,
+        resolution=resolution1,
+    )
+
+
 def combo_change_sign(value: Union[bool, int, float], sign: int):
     """Change direction of bool value given sign (-1 or 1)."""
     if sign not in (1, -1):

src/leaderboard/main.py
@@ -535,8 +535,16 @@ def add_to_llm_and_human_combo_leaderboards(
                         df_forecast0_tmp["resolved_to"].iloc[0], dir0
                     ) * resolution.combo_change_sign(df_forecast1_tmp["resolved_to"].iloc[0], dir1)
                     resolved = (
-                        df_forecast0_tmp["resolved"].iloc[0]
-                        and df_forecast1_tmp["resolved"].iloc[0]
+                        resolution.is_combo_question_resolved(
+                            is_resolved0=df_forecast0_tmp["resolved"].iloc[0],
+                            is_resolved1=df_forecast1_tmp["resolved"].iloc[0],
+                            dir0=dir0,
+                            dir1=dir1,
+                            resolution0=df_forecast0_tmp["resolved_to"].iloc[0],
+                            resolution1=df_forecast1_tmp["resolved_to"].iloc[0],
+                        )
+                        if source in resolution.MARKET_SOURCES
+                        else True
                     )
                     imputed = (
                         df_forecast0_tmp["imputed"].iloc[0] or df_forecast1_tmp["imputed"].iloc[0]

src/resolve_forecasts/markets.py
@@ -101,34 +101,34 @@ def resolve(source, df, dfq, dfr):
     df_standard.sort_values(by=["id", "resolution_date"], inplace=True, ignore_index=True)
 
     # Setup combo resolutions given df_standard
-    def update_col(index, id0, id1, dir0, dir1, col):
-        value_id0 = df_standard.loc[df_standard["id"] == id0, col].iloc[0]
-        value_id1 = df_standard.loc[df_standard["id"] == id1, col].iloc[0]
+    def update_col(index, value0, value1, dir0, dir1, col):
         df_combo.at[index, col] = resolution.combo_change_sign(
-            value_id0, dir0
-        ) * resolution.combo_change_sign(value_id1, dir1)
-        resolved_id0 = df_standard.loc[df_standard["id"] == id0, "resolved"].iloc[0]
-        resolved_id1 = df_standard.loc[df_standard["id"] == id1, "resolved"].iloc[0]
-        df_combo.at[index, "resolved"] = resolved_id0 and resolved_id1
+            value0, dir0
+        ) * resolution.combo_change_sign(value1, dir1)
 
     for index, row in df_combo.iterrows():
         id0, id1 = row["id"]
+        id0_data = df_standard[df_standard["id"] == id0].iloc[0]
+        id1_data = df_standard[df_standard["id"] == id1].iloc[0]
         dir0, dir1 = row["direction"]
-        update_col(
-            index=index,
-            id0=id0,
-            id1=id1,
+
+        for col in ["resolved_to", "market_value_on_due_date"]:
+            update_col(
+                index=index,
+                value0=id0_data[col],
+                value1=id1_data[col],
+                dir0=dir0,
+                dir1=dir1,
+                col=col,
+            )
+
+        df_combo.at[index, "resolved"] = resolution.is_combo_question_resolved(
+            is_resolved0=id0_data["resolved"],
+            is_resolved1=id1_data["resolved"],
             dir0=dir0,
             dir1=dir1,
-            col="resolved_to",
-        )
-        update_col(
-            index=index,
-            id0=id0,
-            id1=id1,
-            dir0=dir0,
-            dir1=dir1,
-            col="market_value_on_due_date",
+            resolution0=id0_data["resolved_to"],
+            resolution1=id1_data["resolved_to"],
         )
 
     df_combo.sort_values(by=["id", "resolution_date"], inplace=True, ignore_index=True)

commit 153a7a03bbf3a0edff74c78e1a66273cff784a94

resolution: fix bug introduced in f6d2387
- account for nullified questions that have resolved to np.nan
- also simplify helper function
- rename parameter `resolution` to `resolved_to`

src/helpers/resolution.py
@@ -8,6 +8,7 @@ import sys
 import tempfile
 from typing import Union
 
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
@@ -58,29 +59,38 @@ def is_combo(row):
     raise ValueError(f"Problem in `is_combo` with {row}. This type is not handled: {type(row)}")
 
 
-def _is_combo_question_resolved_helper(is_resolved, direction, resolution):
+def _is_combo_question_resolved_helper(is_resolved, direction, resolved_to):
     """Determine whether or not a combo question has resolved for a single direction."""
-    if is_resolved and ((direction == 1 and not resolution) or (direction == -1 and resolution)):
-        return True
-    return False
+    return is_resolved and (
+        (direction == 1 and not resolved_to) or (direction == -1 and resolved_to)
+    )
 
 
-def is_combo_question_resolved(is_resolved0, is_resolved1, dir0, dir1, resolution0, resolution1):
+def is_combo_question_resolved(is_resolved0, is_resolved1, dir0, dir1, resolved_to0, resolved_to1):
     """Determine whether or not a combo question has resolved.
 
-    Combo questions are asked in 4 directions: (1,1), (1,-1), (-1,1), (-1,-1). If q2 has resolved,
-    then 2 of the directions have resolved as well. e.g. if q2 resolves No, then questions with
-    directions (1,1) and (-1,1) have resolved to 0. No matter the outcome of q1, the score for these
-    two questions will not change.
+    Combo questions are asked in 4 directions: (1,1), (1,-1), (-1,1), (-1,-1).
+
+    If neither question has resolved, the combo question has not resolved. If both have resolved,
+    the combo question has resolved.
+
+    However, if only one question has resolved, then 2 of the 4 directions of the combo question
+    have resolved. e.g. if q2 resolves No, then questions with directions (1,1) and (-1,1) have
+    resolved to 0; no matter the outcome of q1, the score for these two questions will not change.
     """
-    return _is_combo_question_resolved_helper(
-        is_resolved=is_resolved0,
-        direction=dir0,
-        resolution=resolution0,
-    ) or _is_combo_question_resolved_helper(
-        is_resolved=is_resolved1,
-        direction=dir1,
-        resolution=resolution1,
+    if (is_resolved0 and is_resolved1) or np.isnan(resolved_to0) or np.isnan(resolved_to1):
+        return True
+    return bool(
+        _is_combo_question_resolved_helper(
+            is_resolved=is_resolved0,
+            direction=dir0,
+            resolved_to=resolved_to0,
+        )
+        or _is_combo_question_resolved_helper(
+            is_resolved=is_resolved1,
+            direction=dir1,
+            resolved_to=resolved_to1,
+        )
     )
 
 

src/leaderboard/main.py
@@ -557,8 +557,8 @@ def add_to_llm_and_human_combo_leaderboards(
                             is_resolved1=df_forecast1_tmp["resolved"].iloc[0],
                             dir0=dir0,
                             dir1=dir1,
-                            resolution0=df_forecast0_tmp["resolved_to"].iloc[0],
-                            resolution1=df_forecast1_tmp["resolved_to"].iloc[0],
+                            resolved_to0=df_forecast0_tmp["resolved_to"].iloc[0],
+                            resolved_to1=df_forecast1_tmp["resolved_to"].iloc[0],
                         )
                         if source in resolution.MARKET_SOURCES
                         else True

src/resolve_forecasts/markets.py
@@ -127,8 +127,8 @@ def resolve(source, df, dfq, dfr):
             is_resolved1=id1_data["resolved"],
             dir0=dir0,
             dir1=dir1,
-            resolution0=id0_data["resolved_to"],
-            resolution1=id1_data["resolved_to"],
+            resolved_to0=id0_data["resolved_to"],
+            resolved_to1=id1_data["resolved_to"],
         )
 
     df_combo.sort_values(by=["id", "resolution_date"], inplace=True, ignore_index=True)