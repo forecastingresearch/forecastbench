forecastingresearch/forecastbench

commit bbd8b12cc05d7bdea9f2bfc5515dc3e78dc4691d

llm forecasts: fix bug
* The LLM org was set in a disorganized manner. Centralize this in `constants.py`.
* fix typo in comment

src/helpers/constants.py
@@ -95,43 +95,55 @@ GOOGLE_SOURCE = "GOOGLE"
 MISTRAL_SOURCE = "MISTRAL"
 XAI_SOURCE = "XAI"
 
+OAI_ORG = "OpenAI"
+ANTHROPIC_ORG = "Anthropic"
+META_ORG = "Meta"
+GOOGLE_ORG = "Google"
+
 ZERO_SHOT_AND_SCRATCHPAD_MODELS = {
     # oai context window from: https://platform.openai.com/docs/models/
     "o1-mini-2024-09-12": {
         "source": OAI_SOURCE,
+        "org": OAI_ORG,
         "full_name": "o1-mini-2024-09-12",
         "token_limit": 128000,
     },
     # together.ai context window from: https://docs.together.ai/docs/serverless-models
     "llama-3p3-70B-Instruct-Turbo": {
         "source": TOGETHER_AI_SOURCE,
+        "org": META_ORG,
         "full_name": "meta-llama/Llama-3.3-70B-Instruct-Turbo",
         "token_limit": 131072,
     },
     # anthropic context window from: https://docs.anthropic.com/en/docs/about-claude/models
     "claude-3-5-sonnet-20241022": {
         "source": ANTHROPIC_SOURCE,
+        "org": ANTHROPIC_ORG,
         "full_name": "claude-3-5-sonnet-20241022",
         "token_limit": 200000,
     },
     # google context window from: https://ai.google.dev/gemini-api/docs/models/gemini
     "gemini-2.0-flash-exp": {
         "source": GOOGLE_SOURCE,
+        "org": GOOGLE_ORG,
         "full_name": "gemini-2.0-flash-exp",
         "token_limit": 1048576,
     },
 }
 
 MODEL_TOKEN_LIMITS = dict()
+MODEL_NAME_TO_ORG = dict()
 MODEL_NAME_TO_SOURCE = dict()
 ZERO_SHOT_AND_SCRATCHPAD_MODELS_BY_SOURCE = defaultdict(dict)
 for key, value in ZERO_SHOT_AND_SCRATCHPAD_MODELS.items():
     MODEL_TOKEN_LIMITS[value["full_name"]] = value["token_limit"]
     MODEL_NAME_TO_SOURCE[value["full_name"]] = value["source"]
+    MODEL_NAME_TO_ORG[value["full_name"]] = value["org"]
     ZERO_SHOT_AND_SCRATCHPAD_MODELS_BY_SOURCE[value["source"]][key] = value
 
-# "gpt-4o-mini" Model used by metadat functions in question_curation.METADATA_MODEL_NAME
+# "gpt-4o-mini" Model used by metadata functions in question_curation.METADATA_MODEL_NAME
 MODEL_TOKEN_LIMITS["gpt-4o-mini"] = 128000
+MODEL_NAME_TO_ORG["gpt-4o-mini"] = OAI_ORG
 MODEL_NAME_TO_SOURCE["gpt-4o-mini"] = OAI_SOURCE
 
 # remove models with less than ~17000 token limits

src/helpers/model_eval.py
@@ -73,6 +73,18 @@ def infer_model_source(model_name):
     return constants.MODEL_NAME_TO_SOURCE[model_name]
 
 
+def get_model_org(model_name):
+    """
+    Get the model org given the model.
+
+    Args:
+    - model_name (str): The name of the model.
+    """
+    if model_name not in constants.MODEL_NAME_TO_ORG:
+        raise ValueError(f"Invalid model name: {model_name}")
+    return constants.MODEL_NAME_TO_ORG[model_name]
+
+
 def get_response_with_retry(api_call, wait_time, error_msg):
     """
     Make an API call and retry on failure after a specified wait time.
@@ -668,24 +680,7 @@ def generate_final_forecast_files(forecast_due_date, prompt_type, models, test_o
             final_dir += "_test"
         file_path = f"/tmp/{prompt_type}/{final_dir}/{model}"
         questions = data_utils.read_jsonl(file_path)
-        if "gpt" in model or "o1" in model:
-            org = "OpenAI"
-        elif "llama" in model:
-            org = "Meta"
-        elif "mistral" in model:
-            org = "Mistral AI"
-        elif "claude" in model:
-            org = "Anthropic"
-        elif "qwen" in model:
-            org = "Qwen"
-        elif "gemini" in model:
-            org = "Google"
-        elif "grok" in model:
-            org = "xAI"
-        else:
-            message = f"Unable to find org for model `{model}`."
-            logger.error(message)
-            raise ValueError(message)
+        org = get_model_org(model)
 
         directory = f"/tmp/{prompt_type}/final_submit"
         if test_or_prod == "TEST":

commit cce44b5770440ccc10dc224315b1fc392cf00d41

llm baselines: adjust model to org dict
fix bug introduced in bbd8b12

src/helpers/constants.py
@@ -119,13 +119,13 @@ ZERO_SHOT_AND_SCRATCHPAD_MODELS = {
         "full_name": "meta-llama/Llama-3.3-70B-Instruct-Turbo",
         "token_limit": 131072,
     },
-    "deepseek-ai/DeepSeek-R1": {
+    "DeepSeek-R1": {
         "source": TOGETHER_AI_SOURCE,
         "org": DEEPSEEK_ORG,
         "full_name": "deepseek-ai/DeepSeek-R1",
         "token_limit": 128000,
     },
-    "deepseek-ai/DeepSeek-V3": {
+    "DeepSeek-V3": {
         "source": TOGETHER_AI_SOURCE,
         "org": DEEPSEEK_ORG,
         "full_name": "deepseek-ai/DeepSeek-V3",
@@ -154,6 +154,7 @@ for key, value in ZERO_SHOT_AND_SCRATCHPAD_MODELS.items():
     MODEL_TOKEN_LIMITS[value["full_name"]] = value["token_limit"]
     MODEL_NAME_TO_SOURCE[value["full_name"]] = value["source"]
     MODEL_NAME_TO_ORG[value["full_name"]] = value["org"]
+    MODEL_NAME_TO_ORG[key] = value["org"]
     ZERO_SHOT_AND_SCRATCHPAD_MODELS_BY_SOURCE[value["source"]][key] = value
 
 # "gpt-4o-mini" Model used by metadata functions in question_curation.METADATA_MODEL_NAME