forecastingresearch/forecastbench

commit 2c78ae71acd7b030f45a3ea4e700955c8ea319eb

mirror dataset repo to hugging face
#128

src/helpers/git.py
@@ -40,13 +40,14 @@ def clone(repo_url):
     return repo, local_repo_dir, tmp_key_file_path
 
 
-def clone_and_push_files(repo_url, files, commit_message):
+def clone_and_push_files(repo_url, files, commit_message, mirrors):
     """Clone a repository and push the files to it.
 
     Params:
     - repo_url: something like `git@github.com:forecastingresearch/...`
     - files: a dict of files to push; the key is the local path, the value is the repository path
     - commit_message: commit message
+    - mirrors: a list of repository mirrors to push to
     """
     repo, local_repo_dir, tmp_key_file_path = clone(repo_url=repo_url)
 
@@ -61,13 +62,21 @@ def clone_and_push_files(repo_url, files, commit_message):
     error_encountered = False
     author = Actor("ForecastBench bot", "benchmark@forecastingresearch.org")
     committer = Actor("ForecastBench bot", "benchmark@forecastingresearch.org")
+    ssh_env = {"GIT_SSH_COMMAND": f"ssh -i {tmp_key_file_path} -o StrictHostKeyChecking=no"}
     try:
         repo.index.commit(commit_message, author=author, committer=committer)
         origin = repo.remote(name="origin")
-        origin.push(env={"GIT_SSH_COMMAND": f"ssh -i {tmp_key_file_path}"})
+        origin.push(env=ssh_env)
+        for index, mirror_url in enumerate(mirrors):
+            mirror = repo.create_remote(f"mirror_{index}", url=mirror_url)
+            mirror.push(env={"GIT_SSH_COMMAND": f"ssh -i {tmp_key_file_path}"})
+            mirror.push(env=ssh_env)
+            repo.delete_remote(mirror.name)
+            logger.info(f"Pushed to {mirror_url} (mirror) with commit message: {commit_message}")
     except Exception as e:
         error_encountered = True
-        logger.error(f"encountered error when pushing to git: {e.message}")
+        message = e.message if hasattr(e, "message") else str(e)
+        logger.error(f"encountered error when pushing to git: {message}")
 
     os.remove(tmp_key_file_path)
     shutil.rmtree(local_repo_dir, ignore_errors=True)

src/curate_questions/publish_question_set/main.py
@@ -51,6 +51,8 @@ def driver(_):
         os.remove(soft_link_filename)
     os.symlink(question_set, soft_link_filename)
 
+    mirrors = keys.get_secret_that_may_not_exist("HUGGING_FACE_REPO_URL")
+    mirrors = [mirrors] if mirrors else []
     git.clone_and_push_files(
         repo_url=keys.API_GITHUB_DATASET_REPO_URL,
         files={
@@ -58,6 +60,7 @@ def driver(_):
             soft_link_filename: f"{remote_folder}/{soft_link_question_set}",
         },
         commit_message=f"publish {question_set}.",
+        mirrors=mirrors,
     )
 
     logger.info("Done.")

src/leaderboard/main.py
@@ -1045,10 +1045,13 @@ def driver(_):
     for res in results:
         files.update(res)
 
+    mirrors = keys.get_secret_that_may_not_exist("HUGGING_FACE_REPO_URL")
+    mirrors = [mirrors] if mirrors else []
     git.clone_and_push_files(
         repo_url=keys.API_GITHUB_DATASET_REPO_URL,
         files=files,
         commit_message="leaderboard: automatic update html & csv files.",
+        mirrors=mirrors,
     )
 
 

src/resolve_forecasts/main.py
@@ -96,12 +96,15 @@ def upload_resolution_set(df, forecast_due_date, question_set_filename):
             destination_folder=upload_folder,
         )
         logger.info(f"Uploaded Resolution File {local_filename} to {upload_folder}.")
+        mirrors = keys.get_secret_that_may_not_exist("HUGGING_FACE_REPO_URL")
+        mirrors = [mirrors] if mirrors else []
         git.clone_and_push_files(
             repo_url=keys.API_GITHUB_DATASET_REPO_URL,
             files={
                 local_filename: f"{upload_folder}/{basename}",
             },
             commit_message=f"resolution set: automatic update for {question_set_filename}.",
+            mirrors=mirrors,
         )
 
 

commit b25ab555237077d3a76ecb19205d4da00ee18a17

fix typo introduced in 2c78ae71acd7b030f45a3ea4e700955c8ea319eb

src/helpers/git.py
@@ -69,7 +69,6 @@ def clone_and_push_files(repo_url, files, commit_message, mirrors):
         origin.push(env=ssh_env)
         for index, mirror_url in enumerate(mirrors):
             mirror = repo.create_remote(f"mirror_{index}", url=mirror_url)
-            mirror.push(env={"GIT_SSH_COMMAND": f"ssh -i {tmp_key_file_path}"})
             mirror.push(env=ssh_env)
             repo.delete_remote(mirror.name)
             logger.info(f"Pushed to {mirror_url} (mirror) with commit message: {commit_message}")