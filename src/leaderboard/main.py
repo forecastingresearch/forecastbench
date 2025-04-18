leaderboard: fix bug in computation of imputed percentage
* masks were missing

src/leaderboard/main.py
@@ -112,7 +112,9 @@ def get_leaderboard_entry(df):
     )
 
     # % imputed
-    pct_imputed = int(np.round(df["imputed"].mean() * 100))
+    pct_imputed = int(
+        np.round(df[(data_mask & resolved_mask) | market_mask]["imputed"].mean() * 100)
+    )
 
     return {
         "data": data_resolved_score,