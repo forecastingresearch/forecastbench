c03f9f88ba99ffb051a5a40b6f58105d63055d37
Houtan Bastani <houtan@forecastingresearch.org>
2026-02-16 13:22:34 +0100

    polymarket: increase timeout

--- src/nightly_update_workflow/manager/main.py
+++ src/nightly_update_workflow/manager/main.py
@@ -105,7 +105,7 @@ def main():
     operation_compress_forecast_sets_bucket = compress_bucket(bucket=env.FORECAST_SETS_BUCKET)
 
     dict_to_use = "fetch_and_update"
-    timeout_fetch_and_update = cloud_run.timeout_1h * 5
+    timeout_fetch_and_update = cloud_run.timeout_1h * 6
     task_count = len(question_curation.FREEZE_QUESTION_DATA_SOURCES) + len(
         question_curation.FREEZE_QUESTION_MARKET_SOURCES
     )
--- src/nightly_update_workflow/worker/main.py
+++ src/nightly_update_workflow/worker/main.py
@@ -97,7 +97,7 @@ def get_fetch_and_update():
         ]
     return [
         [
-            (f"func-data-{source}-fetch", True, cloud_run.timeout_1h * 2, 1),
+            (f"func-data-{source}-fetch", True, cloud_run.timeout_1h * 3, 1),
             (f"func-data-{source}-update-questions", True, cloud_run.timeout_1h * 3, 1),
         ]
         for source in sources
